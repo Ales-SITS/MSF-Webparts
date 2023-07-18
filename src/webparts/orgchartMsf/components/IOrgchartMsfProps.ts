2adae33f5ca47f019ffcfc7d1cac908672357f8c
Ales SITS <ales.bachtik@sits.msf.org>
2023-07-18 16:42:14 +0200

    initial

---
+++ src/webparts/orgchartMsf/components/IOrgchartMsfProps.ts
@@ -0,0 +1,7 @@
+export interface IOrgchartMsfProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/orgchartMsf/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IOrgchartMsfWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'OrgchartMsfWebPartStrings' {
+  const strings: IOrgchartMsfWebPartStrings;
+  export = strings;
+}
